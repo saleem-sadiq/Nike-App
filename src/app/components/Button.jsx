c19c9dfe8c15c4732955be3b8f81610ba43cc4f2
Nabeel-Sadiq <sadiqnabeel4@gmail.com>
2023-09-09 23:50:22 +0500

    Subscribe section done

--- src/app/components/Button.jsx
+++ src/app/components/Button.jsx
@@ -1,11 +1,11 @@
 import Image from "next/image"
 
-const Button = ({ label, iconUrl, backgroundColor, borderColor, textColor }) => {
+const Button = ({ label, iconUrl, backgroundColor, borderColor, textColor, fullWidth }) => {
   return (
     <button className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none rounded-full
     ${backgroundColor
         ? `${backgroundColor} ${borderColor} ${textColor}`
-        : "bg-coral-red border-coral-red text-white"}`}>
+        : "bg-coral-red border-coral-red text-white"} ${fullWidth && 'w-full'}`}>
       {label}
       {iconUrl && <Image src={iconUrl} alt="right arrow icon" width={20} height={20} className="rounded-full ml-2" />}
     </button>
